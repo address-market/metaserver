576c32e6752ea2c31f43cd565fead9246cf1bc91
memexpo <dmitry@marscolony.io>
2023-07-08 14:46:32 +0200

    address on image

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import express from 'express';
 import path from 'path';
 import fs from 'fs';
 import cors from 'cors';
+import { createCanvas, loadImage, registerFont } from 'canvas';
+import { isValidEthereumAddress } from './helpers';
+
+registerFont(path.join(__dirname, '../assets', 'OpenSans-Regular.ttf'), {
+  family: 'Open Sans',
+});
 
 const app = express();
 app.use(cors());
@@ -14,12 +20,43 @@ app.use((req: express.Request, res: express.Response, next: Function) => {
 
 app.get('/:addr.png', (req: express.Request, res: express.Response) => {
   const { addr } = req.params; // token is address
+  if (!isValidEthereumAddress(addr)) {
+    res.status(404).end();
+    return;
+  }
+  const canvas = createCanvas(512, 512);
+  const context = canvas.getContext('2d');
+  loadImage(path.join(__dirname, '../assets', 'default.png')).then((image) => {
+    context.drawImage(image, 0, 0, 512, 512);
+    context.font = '20px Open Sans';
+    context.fillStyle = '#FFFFFF';
+    context.strokeStyle = '#000000';
+    context.lineWidth = 3;
+
+    // Position and draw the text on the canvas
+    const textX = canvas.width / 2 - context.measureText(addr).width / 2; // Center the text horizontally
+    const textY = canvas.height / 2 + 220; // Center the text vertically with some offset
+    context.strokeText(addr, textX, textY);
+    context.fillText(addr, textX, textY);
+
+    // Convert the canvas to a PNG buffer
+    const buffer = canvas.toBuffer('image/png');
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': buffer.length,
+    });
+    res.end(buffer);
+  });
   // TODO check if address is valid
-  res.sendFile(path.join(__dirname, '../assets', 'default.png'));
+
 });
 
 app.get('/:token', (req: express.Request, res: express.Response) => {
   const { token } = req.params; // token is address
+  if (!isValidEthereumAddress(token)) {
+    res.status(404).end();
+    return;
+  }
   // TODO check if address is valid
   const domain = req.get('host');
   const protocol = req.protocol;
