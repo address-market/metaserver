c284ff6835b807e26fa5a21f235bbadddd4f016e
root <root@877ddf4e261a>
2024-01-19 14:55:10 +0300

    improvements

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,10 +1,9 @@
-import { BigNumber } from "ethers";
+import { checksumAddress } from "viem";
 
 export const isValidEthereumAddress = (address: string): boolean => {
   return /^0x[a-fA-F0-9]{40}$/.test(address);
 };
 
 export const uint256ToAddress = (uint256: string): string => {
-  const bnInHex = BigNumber.from(uint256).toHexString();
-  return '0x' + bnInHex.slice(2).padStart(40, '0');
+  return checksumAddress(('0x' + BigInt(uint256).toString(16).padStart(40, '0')) as `0x${string}`);
 };
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import cors from 'cors';
 import { createCanvas, loadImage, registerFont } from 'canvas';
 import { isValidEthereumAddress, uint256ToAddress } from './helpers';
+import { checksumAddress } from 'viem';
 
 registerFont(path.join(__dirname, '../assets', 'OpenSans-Regular.ttf'), {
   family: 'Open Sans',
@@ -18,25 +19,38 @@ app.use((req: express.Request, res: express.Response, next: Function) => {
 });
 
 app.get('/:addr.png', (req: express.Request, res: express.Response) => {
-  const { addr } = req.params; // token is address
-  if (!isValidEthereumAddress(addr)) {
+  const { addr: _addr } = req.params; // token is address
+  const bound = Boolean(req.query.bound);
+  if (!isValidEthereumAddress(_addr)) {
     res.status(404).end();
     return;
   }
+  const addr = checksumAddress(_addr as `0x${string}`);
+  const addressPart1 = addr.slice(0, 22);
+  const addressPart2 = addr.slice(22);
+  // console.log('addressPart1', addressPart1);
+  // console.log('addressPart2', addressPart2);
   const canvas = createCanvas(512, 512);
   const context = canvas.getContext('2d');
-  loadImage(path.join(__dirname, '../assets', 'default.png')).then((image) => {
+  loadImage(path.join(__dirname, '../assets', bound ? 'bound.png' : 'default.png')).then((image) => {
     context.drawImage(image, 0, 0, 512, 512);
-    context.font = '20px Open Sans';
-    context.fillStyle = '#FFFFFF';
-    context.strokeStyle = '#000000';
-    context.lineWidth = 3;
+    context.font = '28px Open Sans';
+    context.fillStyle = '#132939';
+    // context.strokeStyle = '#000000';
+    // context.lineWidth = 2;
 
-    // Position and draw the text on the canvas
-    const textX = canvas.width / 2 - context.measureText(addr).width / 2; // Center the text horizontally
-    const textY = canvas.height / 2 + 220; // Center the text vertically with some offset
-    context.strokeText(addr, textX, textY);
-    context.fillText(addr, textX, textY);
+    {
+      const textX = canvas.width / 2 - context.measureText(addressPart1).width / 2;
+      const textY = canvas.height / 2 + 145;
+      // context.strokeText(addressPart1, textX, textY);
+      context.fillText(addressPart1, textX, textY);
+    }
+    {
+      const textX = canvas.width / 2 - context.measureText(addressPart2).width / 2;
+      const textY = canvas.height / 2 + 175;
+      // context.strokeText(addressPart2, textX, textY);
+      context.fillText(addressPart2, textX, textY);
+    }
 
     // Convert the canvas to a PNG buffer
     const buffer = canvas.toBuffer('image/png');
@@ -52,6 +66,12 @@ app.get('/:addr.png', (req: express.Request, res: express.Response) => {
 
 app.get('/:token', (req: express.Request, res: express.Response) => {
   const { token } = req.params;
+  try {
+    BigInt(token);
+  } catch (e) {
+    res.status(404).end();
+    return;
+  }
   const address = uint256ToAddress(token);
   if (!isValidEthereumAddress(address)) {
     res.status(404).end();
@@ -61,8 +81,32 @@ app.get('/:token', (req: express.Request, res: express.Response) => {
   const domain = req.get('host');
   const protocol = req.protocol;
   res.json({
-    description: `NFT Address Option for ${address}`,
+    description: `Address NFT for ${address}`,
+    name: `Address NFT ${address}`,
     image: `${protocol}://${domain}/${address}.png`,
+  });
+});
+
+app.get('/bound/:token', (req: express.Request, res: express.Response) => {
+  const { token } = req.params;
+  try {
+    BigInt(token);
+  } catch (e) {
+    res.status(404).end();
+    return;
+  }
+  const address = uint256ToAddress(token);
+  if (!isValidEthereumAddress(address)) {
+    res.status(404).end();
+    return;
+  }
+  // TODO check if address is valid
+  const domain = req.get('host');
+  const protocol = req.protocol;
+  res.json({
+    description: `Bound Address NFT for ${address}. Not transferable and not sellable.`,
+    name: `Bound Address NFT ${address}`,
+    image: `${protocol}://${domain}/${address}.png?bound=1`,
   });
 });
 
