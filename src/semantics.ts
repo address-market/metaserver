cdab51e7b40118b650e65aae57a8a102381cacde
root <root@877ddf4e261a>
2024-01-29 01:57:21 +0300

    address semantics

--- src/semantics.ts
+++ src/semantics.ts
@@ -1,35 +1,39 @@
 
 const leadingSymbolToName: Record<string, string> = {
   '0': 'zeroes',
-  '1': 'ones',
-  '2': 'twos',
-  '3': 'threes',
-  '4': 'fours',
-  '5': 'fives',
-  '6': 'sixes',
-  '7': 'sevens',
-  '8': 'eights',
-  '9': 'nines',
+  '1': 'numbers',
+  '2': 'numbers',
+  '3': 'numbers',
+  '4': 'numbers',
+  '5': 'numbers',
+  '6': 'numbers',
+  '7': 'numbers',
+  '8': 'numbers',
+  '9': 'numbers',
 
-  a: 'As',
-  b: 'Bs',
-  c: 'Cs',
-  d: 'Ds',
-  e: 'Es',
-  f: 'Fs',
+  a: 'letters',
+  b: 'letters',
+  c: 'letters',
+  d: 'letters',
+  e: 'letters',
+  f: 'letters',
 };
 
 export const getAddressDescription = (address: string): string => {
   // if address has leading symbols
   let leadingSymbol = address[2].toLowerCase();
   for (let i = 0; i < 40; i++) {
-    if (address[i + 2] !== leadingSymbol.toLowerCase()) {
+    if (address[i + 2].toLowerCase() !== leadingSymbol.toLowerCase()) {
       if (i < 5) {
         break;
       }
       return `${i} leading ${leadingSymbolToName[leadingSymbol.toLowerCase()] ?? leadingSymbol}`;
     }
   }
+
+  // if (address.toLowerCase().startsWith('0x3aeba1')) {
+  //   return 'Заебал';
+  // }
 
   return '';
 };
